9dbb19d15cb9525aceaf8c8fdf184f60c79a42c1
Databases-Done-Right <consulting@databasesdoneright.com>
2023-11-04 07:22:22 -0600

    Assignment 1 - Overall Template Formatted

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+var path = require('path')
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
@@ -37,3 +38,9 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+app.get("/", (req, res) => {
+  res.render(path.join(__dirname, './views', 'index.ejs'))
+})
+
+app.use(express.static(path.join(__dirname, '/public')))
